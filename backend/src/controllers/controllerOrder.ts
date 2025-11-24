ef8bb164f9a8582e8d784ff79086fab559571620
Viktor-Bolotin <145365291+Viktor-Bolotin@users.noreply.github.com>
2025-11-24 12:15:07 +0300

    Исправлены коды сервера

--- backend/src/controllers/controllerOrder.ts
+++ backend/src/controllers/controllerOrder.ts
@@ -36,7 +36,7 @@ const createOrder = async (
   }
 
   const orderId = faker.string.uuid();
-  return res.status(201).send({
+  return res.status(200).send({
     id: orderId,
     total: countTotal,
   });
